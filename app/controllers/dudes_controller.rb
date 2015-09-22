f6606db72cb698c446545b6a8be623d4546a8b12
Jonathan Bias <bonjias@gmail.com>
2015-09-22 19:03:33 +0000

    mailers added

--- app/controllers/dudes_controller.rb
+++ app/controllers/dudes_controller.rb
@@ -6,4 +6,15 @@ class DudesController < ApplicationController
 	def new
 		@dude = Dude.new 
 	end 
+
+	def create
+    Dude.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:dude).permit(:name, :phone, :inputlattitude,  :inputlongitude )
+  end
 end
---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,7 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "no-reply@dispatchappbonjias.com"
+
+  def dude_posted
+  end 
+
+end
