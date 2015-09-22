79e414648452c85178c057efef6d85b0f1098188
Jonathan Bias <bonjias@gmail.com>
2015-09-22 19:08:05 +0000

    lesson 40

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,8 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@dispatchappbonjias.com"
 
   def dude_posted
+  	mail(to: "bonjias@gmail.com",
+         subject: "A comment has been added to your place")
   end 
 
 end
